db685a8404db111609e28949d6983b638e22fbd2
ckamiuwu <cami.panchillo@gmail.com>
2026-01-03 20:17:46 -0300

    Actualización de portafolio

--- src/app/content/branding/branding.component.ts
+++ src/app/content/branding/branding.component.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-branding',
   imports: [],
   templateUrl: './branding.component.html',
-  styleUrl: './branding.component.scss'
+  styleUrl: './branding.component.scss',
 })
 export class BrandingComponent {
+  activeImg: string = 'assets/img/PortadaNisum.jpg';
+  imagesSrc: string[] = [
+    'assets/img/PortadaNisum.jpg',
+    'assets/img/Revista_Nisum.jpg',
+    'assets/img/Tarjetas_Nisum.jpg',
+    'assets/img/Pendon_Nisum.jpg'
+  ];
 
+  changeImage(img: string) {
+    this.activeImg = img;
+  }
 }
